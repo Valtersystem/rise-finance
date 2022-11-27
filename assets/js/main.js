3bcb8b7335bb3d6fad658313a1801b7b944837e0
Valtersystem <valtersantos.ifsp@gmail.com>
2022-11-27 10:22:07 +0000

    Configurando o tailwind

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+/* Tailwind Config */
+
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+              body_background: '#3D3971',
+              white_background: '#F4F4F4',
+              chart_gradient: '#9E59FF',
+              stroke_gradient: '#812DE2',
+              step_gradient: '#4945F8',
+              secondary_text: '#ffffff',
+        }
+        
+      }
+    }
+  }
+
+  /* End -- Tailwind Config */
