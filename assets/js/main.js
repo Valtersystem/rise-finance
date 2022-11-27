29b7f5a6d4902f1d54d90175988590a582cde8bb
Valtersystem <valtersantos.ifsp@gmail.com>
2022-11-27 23:20:35 +0000

    ,

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,13 +7,23 @@ tailwind.config = {
               body_background: '#3D3971',
               white_background: '#F4F4F4',
               chart_gradient: '#9E59FF',
-              stroke_gradient: '#812DE2',
+              stroke_gradient: '#121139',
               step_gradient: '#4945F8',
               secondary_text: '#ffffff',
               footer_background: '#121139',
               text_title_footer: '#92989F',
-        }
-      }
+        },
+        backgroundImage: {
+          'hero-texture': "url('/assets//image/herosection.svg')",
+        },
+        fontSize: {
+          xl90: '5.6rem',
+          xl22: '1.3rem',
+        },
+        lineHeight: {
+          'lh5': '6rem',
+        },
+      },
     }
   }
 
